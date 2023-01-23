e0e208f74c8dc55e5d562eacf55ca52690e02897
Juan Manuel <juanmahc0109@gmail.com>
2023-01-23 10:20:17 -0500

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://juanma010901.github.io/GitHub-Actions/',
   plugins: [react()],
 })
